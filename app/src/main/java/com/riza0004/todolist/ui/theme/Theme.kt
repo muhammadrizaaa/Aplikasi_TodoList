2b883d74655f1beb7193d183997a0e137d50cbc4
Muhammad Riza Rabbani <muhammad_riza_29rpl@student.smktelkom-mlg.sch.id>
2025-03-26 13:59:01 +0700

    First Push + tambah halaman utama, topAppBar, Floating action button

--- app/src/main/java/com/riza0004/todolist/ui/theme/Theme.kt
+++ app/src/main/java/com/riza0004/todolist/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.riza0004.todolist.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
